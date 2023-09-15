c134f45d4911bebe6ab6f1ead64c729776590040
uzair61 <qureshiuzair61@gmail.com>
2023-09-15 17:20:47 +0530

    updated

--- api-gateway-01/src/main/java/com/jhola/ApiGatewayApplication.java
+++ api-gateway-01/src/main/java/com/jhola/ApiGatewayApplication.java
@@ -2,12 +2,16 @@ package com.jhola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-public class ApiGateway01Application {
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGateway01Application.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
+
--- discovery-service-microservices/src/main/java/com/jhola/DiscoveryServiceMicroservicesApplication.java
+++ discovery-service-microservices/src/main/java/com/jhola/DiscoveryServiceMicroservicesApplication.java
@@ -3,8 +3,11 @@ package com.jhola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceMicroservicesApplication {
 
 	public static void main(String[] args) {
